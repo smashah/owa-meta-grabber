164cb28035b149fb82a8b60b2d07d64d8e038311
Mohammed Shah <m9shah3@gmail.com>
2022-03-29 14:25:34 +0000

    fix meta getter for wa group chats

--- api/index.js
+++ api/index.js
@@ -1,5 +1,22 @@
-const meta = require("meta-grabber")
+const cheerio = require("cheerio");
+const axios = require("axios");
 const usage = "https://metagrabber.vercel.app/api?url=https://discord.com"
+
+async function meta(url) {
+      const page = (await axios.get(url)).data;
+      const $ = cheerio.load(page);
+      const title = $('meta[property="og:title"]').attr('content') || $('title').text() || $('meta[name="title"]').attr('content')
+      const description = $('meta[property="og:description"]').attr('content') || $('meta[name="description"]').attr('content')
+      const url = $('meta[property="og:url"]').attr('content')
+      const site_name = $('meta[property="og:site_name"]').attr('content')
+      let image = $('meta[property="og:image"]').attr('content') || $('meta[property="og:image:url"]').attr('content')
+      image = image.replaceAll("amp;","")
+      const icon = $('link[rel="icon"]').attr('href') || $('link[rel="shortcut icon"]').attr('href')
+      const keywords = $('meta[property="og:keywords"]').attr('content') || $('meta[name="keywords"]').attr('content')
+      const json = { title: title, description: description, url: url, site_name: site_name, image: image, icon: icon, keywords: keywords };
+      console.log(json)
+      return json;
+}
 
 module.exports = async (request, response) => {
   const { url } = request.query;
@@ -11,7 +28,7 @@ module.exports = async (request, response) => {
     });
   } else {
     try {
-      let metaRes = meta(url)
+      let metaRes = await meta(url)
       metaRes.success = true
       response.status(200).json(metaRes);
     } catch (error) {
